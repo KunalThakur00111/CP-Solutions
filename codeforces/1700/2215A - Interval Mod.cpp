12989aa4fc931277476d05d6c8f7d10ee0a060e0
Kunal Thakur <kunalx2006@gmail.com>
2026-08-29 16:50:53 +0530

    Add Interval Mod (1700)

---
+++ codeforces/1700/2215A - Interval Mod.cpp
@@ -0,0 +1,215 @@
+#pragma GCC optimize("O3,unroll-loops")
+#include <bits/stdc++.h>
+#include <numeric>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define kunel ios_base::sync_with_stdio(false);cin.tie(nullptr);
+#define ll long long
+#define vl vector<ll>
+#define vc vector<char>
+#define vi vector<int>
+#define vvc vector<vector<char>>
+#define vvi vector<vector<int>>
+#define vvpi vector<vector<pair<int,int>>>
+#define vvb vector<vector<bool>>
+#define vvl vector<vector<ll>>
+#define vvvl vector<vector<vector<ll>>>
+#define vvvi vector<vector<vector<int>>>
+#define vpi vector<pair<int,int>>
+#define vpl vector<pair<ll,ll>>
+#define gl greater<ll>
+#define vp vector<pair<ll,ll>> 
+#define __int128_t i128
+using ull=unsigned long long;
+const int MOD=1e9+7;
+const int MOD2=998244353;
+const int MOD3=676767677;
+const int MAXN=1e6+2;
+const int N=2e5;
+const int MAX=32;
+ll fact[MAXN+1];
+ll invfact[MAXN+1];
+class disjointset{
+    public:
+    vi parent,rank,sz;
+    disjointset(int n){
+        parent.resize(n);
+        rank.resize(n);
+        sz.resize(n,1);
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+        }
+    }
+    int find(int u){
+        if(parent[u]!=u){
+            return parent[u]=find(parent[u]);
+        }
+        return u;
+    }
+    void unionbyrank(int u,int v){
+        int uu=find(u);
+        int vv=find(v);
+        if(uu==vv) return;
+        if(rank[uu]>rank[vv]){
+            parent[vv]=uu;
+        }
+        else if(rank[vv]>rank[uu]){
+            parent[uu]=vv;
+        }
+        else{
+            parent[uu]=vv;
+            rank[vv]++;
+        }
+    }
+    void unionbysize(int u,int v){
+        int uu=find(u);
+        int vv=find(v);
+        if(uu==vv) return;
+        if(sz[uu]>sz[vv]){
+            parent[vv]=uu;
+            sz[uu]+=sz[vv];
+        }
+        else{
+            parent[uu]=vv;
+            sz[vv]+=sz[uu];
+        }
+    }
+};
+ll gcd(ll a, ll b) {
+    while(b!=0){
+        ll t=b;
+        b=a%b;
+        a=t;
+    }
+    return a;
+}
+ll binexp(ll a,ll b){
+    ll res=1;
+    a%=MOD2;
+    while(b){
+        if(b&1) res=res*a%MOD2;
+        a=a*a%MOD2;
+        b>>=1;
+    }
+    return res;
+}
+void precompute(){
+    fact[0]=1;
+    for(int i=1;i<=MAXN;i++){
+        fact[i]=fact[i-1]*i%MOD2;
+    }
+    invfact[MAXN]=binexp(fact[MAXN],MOD2-2);
+    for(int i=MAXN-1;i>=0;i--){
+        invfact[i]=invfact[i+1]*(i+1)%MOD2;
+    }
+}
+ll ncr(int n, int r){
+    if(r<0 || r>n){
+        return 0;
+    }
+    return fact[n]*invfact[r]%MOD2*invfact[n-r]%MOD2;
+}
+ll modinv(ll x){
+    return binexp(x,MOD-2);
+}
+void divisors(ll n,vl& div){
+    for(int i=1;i*i<=n;i++){
+        if(n%i==0){
+            div.emplace_back(i);
+            if(n/i!=i){
+                div.emplace_back(n/i);
+            }
+        }
+    }
+}
+bool isprime(ll n){
+    if(n<2) return false;
+    for(ll i=2;i*i<=n;i++){
+        if(n%i==0) return false;
+    }
+    return true;
+}
+ll power(ll num,ll pow){
+    if(pow==0) return 1;
+    ll base=num%MOD;
+    ll ans=1;
+    while(pow>0){
+        if(pow&1){
+            ans=(base*ans)%MOD;
+        }
+        base=(base*base)%MOD;
+        pow>>=1;
+    }
+    return ans;
+}
+ll helper(int i,int prev,int k,vl& state0,vl& state1,vvl& dp){
+    int n=state0.size();
+    if(i>=n){
+        if(prev==-1) return 1e18;
+        return 0;
+    }
+    if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+    ll mini=1e18;
+    if(prev==-1){
+        ll val0=state0[i]-(i?state0[i-1]:0);
+        ll val1=state1[i]-(i?state1[i-1]:0);
+        mini=min(mini,min(val0,val1)+helper(i+1,-1,k,state0,state1,dp));
+        if(i+k<=n){
+            ll sum0=state0[i+k-1]-(i?state0[i-1]:0);
+            ll sum1=state1[i+k-1]-(i?state1[i-1]:0);
+            mini=min(mini,sum0+helper(i+k,0,k,state0,state1,dp));
+            mini=min(mini,sum1+helper(i+k,1,k,state0,state1,dp));
+        }
+    }
+    else{
+        {// extend prev
+            if(prev==0){
+                mini=min(mini,state0[i]-state0[i-1]+helper(i+1,0,k,state0,state1,dp));
+            }
+            if(prev==1){
+                mini=min(mini,state1[i]-state1[i-1]+helper(i+1,1,k,state0,state1,dp));
+            }
+        }
+        {// start new
+            // prev is %q, now we r starting doing %p%q
+            // prev is %p%q, now we r staring %q
+            if(prev==1){
+                mini=min(mini,state0[i]-state0[i-1]+helper(i+1,0,k,state0,state1,dp));
+            }
+            else{
+                mini=min(mini,state1[i]-state1[i-1]+helper(i+1,1,k,state0,state1,dp));
+            }
+        }
+    }
+    return dp[i][prev+1]=mini;
+}
+void solve(){
+    int n,k,p,q;
+    cin>>n>>k>>p>>q;
+    if(p<q) swap(p,q);
+    vi a(n);
+    for(auto& x: a) cin>>x;
+    vl state1(n),state0(n);
+    state0[0]=a[0]%q;
+    state1[0]=(a[0]%p)%q;
+    for(int i=1;i<n;i++){
+        int val0=a[i]%q;
+        int val1=(a[i]%p)%q;
+        state0[i]=state0[i-1]+val0;
+        state1[i]=state1[i-1]+val1;
+    }
+    vvl dp(n,vl(3,-1));
+    cout<<helper(0,-1,k,state0,state1,dp)<<endl;
+}
+int32_t main(){
+    kunel
+    //precompute();
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
