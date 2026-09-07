ea9a0636d95171a5bbde1523aff96058d1501c47
Kunal Thakur <kunalx2006@gmail.com>
2026-09-07 13:16:05 +0530

    Add Distinct-Subsequences-II (dynamic-programming)

--- leetcode/dynamic-programming/940-Distinct-Subsequences-II.cpp
+++ leetcode/dynamic-programming/940-Distinct-Subsequences-II.cpp
@@ -5,13 +5,13 @@ public:
         vector<int> dp(n+1);
         dp[0]=1;
         int mod=1e9+7;
-        unordered_map<char,int> mp;
+        vector<int> mp(26,-1);
         for(int i=1;i<=n;i++){
             dp[i]=(dp[i]+(2*dp[i-1])%mod)%mod;
-            if(mp.count(s[i-1])){
-                dp[i]=(dp[i]-dp[mp[s[i-1]]-1]+mod)%mod;
+            if(mp[s[i-1]-'a']!=-1){
+                dp[i]=(dp[i]-dp[mp[s[i-1]-'a']-1]+mod)%mod;
             }
-            mp[s[i-1]]=i;
+            mp[s[i-1]-'a']=i;
         }
         return (dp[n]-1+mod)%mod;
     }
